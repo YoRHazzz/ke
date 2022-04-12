4094d00e85671ff86e2a5a5227ba7d1740a1b8fb
YoRHazzz <1245469389@qq.com>
2022-04-13 03:55:09 +0800

    优化filter的实现方式
    2.8s/epochs

--- examples/FB15K_TransE.py
+++ examples/FB15K_TransE.py
@@ -1,4 +1,6 @@
 import os
+import time
+import numpy as np
 
 from ke.data import KGMapping, KGDataset
 from ke import fix_random
@@ -14,7 +16,7 @@ TRAIN_BATCH_SIZE = 128
 VALID_BATCH_SIZE = 64
 TEST_BATCH_SIZE = 64
 FILTER_FLAG = True
-
+USE_GPU = True
 
 if __name__ == "__main__":
     fix_random()
@@ -22,12 +24,23 @@ if __name__ == "__main__":
     train_path = os.path.join(FB15K_path, "train.txt")
     fb15k_mapping = KGMapping(FB15K_path)
 
-    fb15k_train_dataset = KGDataset(train_path, fb15k_mapping, neg_sample_flag=True, filter_flag=True)
+    fb15k_train_dataset = KGDataset(train_path, fb15k_mapping, filter_flag=True)
     fb15k_train_dataloader = DataLoader(fb15k_train_dataset, TRAIN_BATCH_SIZE)
     broken_range = set(range(fb15k_train_dataset.n_entity))
 
+    device = torch.device('cuda') if USE_GPU else torch.device('cpu')
+
     for i in trange(EPOCHS, desc='Train Epochs', mininterval=0.5, unit='epochs'):
-        for head, relation, tail, broken_head, broken_relation, broken_tail in fb15k_train_dataloader:
-            positive_triplets = torch.stack((head, relation, tail), dim=1)
-            negative_triplets = torch.stack((broken_head, broken_relation, broken_tail), dim=1)
-            current_batch_size = len(head)
+        for heads, relations, tails, head_or_tail, negative_entities in fb15k_train_dataloader:
+            current_batch_size = len(heads)
+            # head_or_tail = torch.randint(high=2, size=(current_batch_size,))
+            # negative_entities = torch.randint(high=fb15k_train_dataset.n_entity, size=(current_batch_size,))
+            # heads, relations, tails = heads.to(device), relations.to(device), tails.to(device)
+            # head_or_tail, negative_entities = head_or_tail.to(device), negative_entities.to(device)
+
+            positive_triplets = torch.stack((heads, relations, tails), dim=1)
+
+            negative_heads = torch.where(head_or_tail == 1, negative_entities, heads)
+            negative_tails = torch.where(head_or_tail == 0, negative_entities, tails)
+            negative_triplets = torch.stack((negative_heads, relations, negative_tails), dim=1)
+        fb15k_train_dataset.regenerate_head_or_tail()
--- ke/data/KGDataset.py
+++ ke/data/KGDataset.py
@@ -1,13 +1,15 @@
 import random
+import numpy as np
+import torch
 
 from torch.utils.data import Dataset
 from .KGMapping import KGMapping
 
 
 class KGDataset(Dataset):
-    def __init__(self, file_path, kg_mapping: KGMapping, neg_sample_flag=False, filter_flag=False):
+    def __init__(self, file_path, kg_mapping: KGMapping, filter_flag=False):
+        super(KGDataset, self).__init__()
         self.kg_mapping = kg_mapping
-        self.neg_sample_flag = neg_sample_flag
         self.filter_flag = filter_flag
 
         entity2id, relation2id = self.kg_mapping.entity2id, self.kg_mapping.relation2id
@@ -21,28 +23,24 @@ class KGDataset(Dataset):
         self.n_entity = kg_mapping.n_entity
         self.n_relation = kg_mapping.n_relation
         self.n_triplet = len(self.data)
-        self.entity_set = set(range(self.n_entity))
+        self.head_or_tail = np.random.randint(low=0, high=2, size=(self.n_triplet,))
 
     def __len__(self):
         return self.n_triplet
 
     def __getitem__(self, index):
-        if not self.neg_sample_flag:
-            return self.data[index]
-        else:
-            h, r, t = self.data[index]
-            head_or_tail = random.randint(0, 1)
-            if head_or_tail == 0:
-                negative_head = self.generate_negative(self.kg_mapping.h_of_rt[(r, t)])
-                corrupted_triplet = (negative_head, r, t)
-            else:
-                negative_tail = self.generate_negative(self.kg_mapping.t_of_hr[(h, r)])
-                corrupted_triplet = (h, r, negative_tail)
-            return *self.data[index], *corrupted_triplet
-
-    def generate_negative(self, illegal_set):
-        negative_index = random.randint(0, self.n_entity - 1)
         if self.filter_flag:
-            while negative_index in illegal_set:
-                negative_index = random.randint(0, self.n_entity - 1)
-        return negative_index
+            h, r, t = self.data[index]
+            negative_index = random.randint(0, self.n_entity - 1)
+            if self.head_or_tail[index] == 0:
+                while negative_index in self.kg_mapping.h_of_rt[(r, t)]:
+                    negative_index = random.randint(0, self.n_entity - 1)
+            else:
+                while negative_index in self.kg_mapping.t_of_hr[(h, r)]:
+                    negative_index = random.randint(0, self.n_entity - 1)
+            return *self.data[index], self.head_or_tail[index], negative_index
+        else:
+            return self.data[index]
+
+    def regenerate_head_or_tail(self):
+        self.head_or_tail = np.random.randint(low=0, high=2, size=(self.n_triplet,))
