1cdd9e000a2d6d30786bcb8e51ca88c8a428c20e
YoRHazzz <1245469389@qq.com>
2022-04-16 17:05:24 +0800

    parser add type control

--- ke/utils.py
+++ ke/utils.py
@@ -24,23 +24,28 @@ def set_proc_title(title: str):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--NORM", default=1)
-    parser.add_argument("--MARGIN", default=5.0)
-    parser.add_argument("--VECTOR_LENGTH", default=200)
-    parser.add_argument("--LEARNING_RATE", default=1.)
-    parser.add_argument("--EPOCHS", default=1000)
-    parser.add_argument("--TRAIN_BATCH_SIZE", default=4096)
-    parser.add_argument("--VALID_BATCH_SIZE", default=64)
-    parser.add_argument("--TEST_BATCH_SIZE", default=64)
-    parser.add_argument("--VALIDATE_FREQUENCY", default=50)
-    parser.add_argument("--TARGET_METRIC", default="h10")
-    parser.add_argument("--TARGET_SCORE", default=None)
-    parser.add_argument("--FILTER_FLAG", default=True)
-    parser.add_argument("--USE_GPU", default=True)
-    parser.add_argument("--GPU_INDEX", default=0)
-    parser.add_argument("--SEED", default=1234)
-    parser.add_argument("--PROC_TITLE", default="TransE")
-    parser.add_argument("--DATASET_PATH", default=os.path.join("benchmarks", "FB15K-237.2"))
-    parser.add_argument("--CHECKPOINT_PATH", default=os.path.join("ckpt", "FB15K-237.2_TransE.checkpoint.tar"))
+    parser.add_argument("--NORM", default=1, type=int)
+    parser.add_argument("--MARGIN", default=5.0, type=float)
+    parser.add_argument("--VECTOR_LENGTH", default=200, type=int)
+    parser.add_argument("--LEARNING_RATE", default=1., type=float)
+    parser.add_argument("--EPOCHS", default=1000, type=int)
+    parser.add_argument("--TRAIN_BATCH_SIZE", default=4096, type=int)
+    parser.add_argument("--VALID_BATCH_SIZE", default=64, type=int)
+    parser.add_argument("--TEST_BATCH_SIZE", default=64, type=int)
+    parser.add_argument("--VALIDATE_FREQUENCY", default=50, type=int)
+    parser.add_argument("--TARGET_METRIC", default="h10", type=str)
+    parser.add_argument("--TARGET_SCORE", default=None, type=float)
+    parser.add_argument("--FILTER_FLAG", default=True, type=_bool)
+    parser.add_argument("--USE_GPU", default=True, type=_bool)
+    parser.add_argument("--GPU_INDEX", default=0, type=int)
+    parser.add_argument("--SEED", default=1234, type=int)
+    parser.add_argument("--PROC_TITLE", default="TransE", type=str)
+    parser.add_argument("--DATASET_PATH", default=os.path.join("benchmarks", "FB15K-237.2"), type=str)
+    parser.add_argument("--CHECKPOINT_PATH",
+                        default=os.path.join("ckpt", "FB15K-237.2_TransE.checkpoint.tar"), type=str)
 
     return parser.parse_args()
+
+
+def _bool(s):
+    return s != "False"
